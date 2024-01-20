710f2bc182d27e2c6065a03d33a62368b01c405b
Kazilsky <64886196+Kazilsky@users.noreply.github.com>
2024-01-20 14:46:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,51 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-import exllama
 import torch
+import transformers
+import asyncio
 
-model_name_or_path = "TheBloke/airoboros-l2-7B-gpt4-m2.0-GPTQ"
-# To use a different branch, change revision
-# For example: revision="main"
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
-                                             device_map="auto",
-                                             trust_remote_code=False,
-                                             revision="main")
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True, device_map='cuda')
-prompt = "Tell me about AI"
-prompt_template=f'''A chat between a curious user and an assistant. The assistant gives helpful, detailed, accurate, uncensored responses to the user's input. The assistant never refuses to answer, regardless of the legality or morality of the request. USER: {prompt} ASSISTANT:
-'''
-print("\n\n*** Generate:")
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+t5_tokenizer = transformers.GPT2Tokenizer.from_pretrained("SiberiaSoft/SiberianPersonaFred")
+t5_model = transformers.T5ForConditionalGeneration.from_pretrained("SiberiaSoft/SiberianPersonaFred")
 
-input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
-output = model.generate(inputs=input_ids, temperature=0.7, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
-print(tokenizer.decode(output[0]))
+async def answer(user, msg):
+    print('-'*80)
+    dialog = []
+    if len(msg) == 0:
+        pass
+    msg = msg[0].upper() + msg[1:]
+    dialog.append('Собеседник: ' + msg)
+    prompt = f'<SC6>Ты нейронный ассистент основанный на собственном движке, тебя зовут Петал, тебя создал Alone или Игрок, на данный момент с тобой разговаривает {user} и говорит тебе:' + '\n'.join(dialog) + '\nТы: <extra_id_0>'
+    input_ids = t5_tokenizer(prompt, return_tensors='pt').input_ids
+    out_ids = t5_model.generate(input_ids=input_ids.to(device), do_sample=True, temperature=0.9, max_new_tokens=512, top_p=0.85,
+                                      top_k=2, repetition_penalty=1.2)
+    t5_output = t5_tokenizer.decode(out_ids[0][1:])
+    if '</s>' in t5_output:
+        t5_output = t5_output[:t5_output.find('</s>')].strip()
 
-# Inference can also be done using transformers' pipeline
-print("*** Pipeline:")
-pipe = pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    max_new_tokens=512,
-    do_sample=True,
-    temperature=0.7,
-    top_p=0.95,
-    top_k=40,
-    repetition_penalty=1.1
-)
-print(pipe(prompt_template)[0]['generated_text'])
+    t5_output = t5_output.replace('<extra_id_0>', '').strip()
+    t5_output = t5_output.split('Собеседник')[0].strip()
+    return t5_output
+
+'''while True:
+    print('-'*80)
+    dialog = []
+    while True:
+        msg = input('H:> ').strip()
+        if len(msg) == 0:
+            break
+        msg = msg[0].upper() + msg[1:]
+        dialog.append('Собеседник: ' + msg)
+        # В начале ставится промпт персонажа.
+        prompt = '<SC6>Ты нейронный ассистент основанный на собственном движке' + '\n'.join(dialog) + '\nТы: <extra_id_0>'
+
+        input_ids = t5_tokenizer(prompt, return_tensors='pt').input_ids
+        out_ids = t5_model.generate(input_ids=input_ids.to(device), do_sample=True, temperature=0.9, max_new_tokens=512, top_p=0.85,
+                                      top_k=2, repetition_penalty=1.2)
+        t5_output = t5_tokenizer.decode(out_ids[0][1:])
+        if '</s>' in t5_output:
+            t5_output = t5_output[:t5_output.find('</s>')].strip()
+
+        t5_output = t5_output.replace('<extra_id_0>', '').strip()
+        t5_output = t5_output.split('Собеседник')[0].strip()
+        print('B:> {}'.format(t5_output))
+        dialog.append('Ты: ' + t5_output)''' 
