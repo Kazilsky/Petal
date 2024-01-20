b944474473a625dfb6dff9dad18f4b809e36a11f
Kazilsky <64886196+Kazilsky@users.noreply.github.com>
2024-01-20 16:09:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from fuzzywuzzy import process
 #keep_alive()
 # Настройки дискорда Петал
 #youtube_dl.utils.bug_reports_message = lambda: '' 
-Discordtoken = "MTE4NTYxMjI3NjUyNzU0NjQ3OQ.GXTwNk.usMkyHYDko_AGhNOg6nGtUeVA_qWkBeNniYfDI"
+Discordtoken = ${{ secrets.DISCORDTOKEN }}
 intents = discord.Intents.default()  # Подключаем "Разрешения"
 intents.message_content = True
 description = '''An example bot to showcase the discord.ext.commands extension
