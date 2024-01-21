ce91ac97efbc7c7afe7b65bbf4b504f1c59f3959
Kazilsky <64886196+Kazilsky@users.noreply.github.com>
2024-01-21 17:20:35 +0300

    Update neiro.py

--- neiro.py
+++ neiro.py
@@ -20,7 +20,7 @@ async def answer(user, msg):
     
     print("\n\n*** Generate:")
     input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
-    output = model.generate(inputs=input_ids, temperature=0.7, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
+    output = model.generate(inputs=input_ids, do_sample=True, temperature=0.7, top_p=0.95, top_k=40, max_new_tokens=512)
     print(tokenizer.decode(output[0]))
     
     # Inference can also be done using transformers' pipeline
