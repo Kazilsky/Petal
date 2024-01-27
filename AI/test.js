eb1806df74b961ac36f3e881dcdf776b7ef40907
Kazilsky <64886196+Kazilsky@users.noreply.github.com>
2024-01-27 10:41:52 +0300

    Update test.js

--- AI/test.js
+++ AI/test.js
@@ -5,7 +5,7 @@ function answer(Author, question) {
     args: [Author, question]
     };
 
-    PythonShell.run('./AI/test.py', options).then(messages=>{    
+    PythonShell.run('./AI/neiro.py', options).then(messages=>{    
         console.log('' + message);
     });}
 module.exports = { answer };
