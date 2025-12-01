3e4f7af112486d3be3f53a2467d44474ad8b5a45
Kazilsky <mk9151341600@gmail.com>
2025-12-01 09:52:56 +0500

    Подключение к Ukagaka - model

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { DiscordBot } from './services/discord';
+import { SocketServer } from './services/socket-server';
 
 const bot = new DiscordBot();
-bot.start().catch((error) => {
+const socketServer = new SocketServer(3000); // Socket.IO сервер на порту 3000
+
+Promise.all([
+  bot.start(),
+  socketServer.start()
+]).catch((error) => {
   console.error('Fatal error:', error);
   process.exit(1);
 });
